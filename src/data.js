0098aae0a3c2736bb9f6320564160c966c8767c6
Sachinrawat1215 <Sachinrawat0853@gmail.com>
2023-08-21 02:18:06 +0530

    get and post route created

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        name: 'Darshit Sharma',
+        age: 23,
+        gender: "Male",
+        email: 'darshit@gmail.com'
+    },
+    {
+        name: 'Rohan Kumar',
+        age: 28,
+        gender: "Male",
+        email: 'rohan@gmail.com'
+    },
+    {
+        name: 'Manoj Singh',
+        age: 22,
+        gender: "Male",
+        email: 'manoj@gmail.com'
+    },
+    {
+        name: 'Rohit Sharma',
+        age: 26,
+        gender: "Male",
+        email: 'rohit@gmail.com'
+    },
+    {
+        name: 'Shristi Kumari',
+        age: 24,
+        gender: "Female",
+        email: 'shristi@gmail.com'
+    },
+];
+
+module.exports = data;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const userData = require('./data');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+ try{
+    const data = userData;
+    res.status(200).json({ status: true, message: 'Data fetched!', data });
+ }catch(error){
+    res.status(500).json({status: false, message: 'Failed to fetch data'});
+ }
+});
+
+app.post('/', (req, res) => {
+  try {
+    const data = req.body;
+    res
+      .status(200)
+      .json({ status: true, message: 'Data saved successfully!', data });
+  } catch (error) {
+    res.status(500).json({ status: false, message: 'Failed to post data' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to port number ${PORT}`);
+});
